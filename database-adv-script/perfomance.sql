6c85a8d4cbf8b47c6c1c1507bb8573922fe6a7ab
root <root@DESKTOP-F8D6VBB.localdomain>
2025-05-18 21:31:42 +0100

    refractor

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,9 @@
 -- Initial Query
 SELECT 
+    SELECT 
     b.booking_id,
+    b.start_date,
+    b.end_date,
     u.user_id,
     u.first_name AS user_first_name,
     u.last_name AS user_last_name,
@@ -13,17 +16,23 @@ SELECT
     pay.amount AS payment_amount,
     pay.payment_date,
     pay.payment_method
-
-FROM Booking b
-JOIN "User" u ON b.user_id = u.user_id
-JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
-ORDER BY b.start_date DESC;
+FROM 
+    Booking b
+JOIN 
+    "User" u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+JOIN 
+    Payment pay ON b.booking_id = pay.booking_id
+WHERE
+    pay.amount > 0 AND b.start_date IS NOT NULL;
 
 -- Measure the query performance using EXPLAIN ANALYZE
 EXPLAIN ANALYZE
-SELECT
+SELECT 
     b.booking_id,
+    b.start_date,
+    b.end_date,
     u.user_id,
     u.first_name AS user_first_name,
     u.last_name AS user_last_name,
@@ -36,14 +45,18 @@ SELECT
     pay.amount AS payment_amount,
     pay.payment_date,
     pay.payment_method
+FROM 
+    Booking b
+JOIN 
+    "User" u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+JOIN 
+    Payment pay ON b.booking_id = pay.booking_id
+WHERE
+    pay.amount > 0 AND b.start_date IS NOT NULL;
 
-FROM Booking b
-JOIN "User" u ON b.user_id = u.user_id
-JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
-ORDER BY b.start_date DESC;
-
--- Refactored Query: Optimized query with indexing and removing wide/unused columns
+-- Refactored Query: Optimized query with indexing and removing wide/unused columns and filter/sorting with ORDER BY
 
 CREATE INDEX idx_booking_start_date ON Booking(start_date);     -- For filtering/sorting by start date
 
@@ -63,7 +76,7 @@ JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
 ORDER BY b.start_date DESC;
 
--- Measure the query performance after refractoring using EXPLAIN ANALYZE
+-- Measure the query performance of refractored queryusing EXPLAIN ANALYZE
 EXPLAIN ANALYZE
 SELECT
     b.booking_id,
